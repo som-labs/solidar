ab80d367a1b6e4fb3a7d6f03ab7abe0a1a5dd7a2
David García Garzón <david.garcia@somenergia.coop>
2023-08-26 22:55:42 +0200

    🌐 Added english as language

--- src/components/LanguageMenu.jsx
+++ src/components/LanguageMenu.jsx
@@ -25,6 +25,10 @@ export default function LanguageMenu() {
       text: 'Català',
     },
     {
+      id: 'en',
+      text: 'English',
+    },
+    {
       id: 'es',
       text: 'Español',
     },
