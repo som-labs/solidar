23e0f80bcb3f50203d1a5439b64387a436f14b98
David García Garzón <david.garcia@somenergia.coop>
2023-08-27 17:15:51 +0200

    ✨ Home page mockup

---
+++ src/Home/ArticleThumb.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from  '@mui/material/Typography'
+import Skeleton from  '@mui/material/Skeleton'
+import Card from  '@mui/material/Card'
+import CardContent from  '@mui/material/CardContent'
+import CardActionArea from  '@mui/material/CardActionArea'
+import { Link } from "react-router-dom"
+
+export default function ArticleThumb(params) {
+  const {url, title, excerpt, image} = params
+  return <>
+    <Card elevation={0} sx={{width: '12rem'}}>
+      <CardActionArea {...(url?{component: Link, to: url, target:'_black'}:{})} >
+        <CardContent>
+          <Box sx={{display: 'flex', flexFlow: 'column', width: '10rem'}}>
+            {image ? (
+              <img src={image} width='160px' height='90px' style={{objectFit: 'cover'}}  />
+              ):(
+              <Skeleton variant="rectangular" animation={false} width='160px' height='90px' />
+            )}
+            <Typography variant="body1">{title}</Typography>
+            <Typography variant="body2">{excerpt}</Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  </>
+}
+
+
---
+++ src/Home/ToolSelector.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from  '@mui/material/Typography'
+import Skeleton from  '@mui/material/Skeleton'
+import Card from  '@mui/material/Card'
+import CardContent from  '@mui/material/CardContent'
+import CardActionArea from  '@mui/material/CardActionArea'
+import { Link } from "react-router-dom"
+
+export default function ToolSelector(params) {
+  const {route, title, subtitle} = params
+  return <>
+    <Card elevation={0}>
+      <CardActionArea {...(route?{component: Link, to: route}:{})} >
+        <CardContent>
+          <Box sx={{display: 'flex', flexFlow: 'column', alignItems: 'center'}}>
+            <Skeleton variant="rectangular" animation={false} width={310} height={400}/>
+            <Typography variant="h5" sx={{textAlign: 'center'}}>{title}</Typography>
+            <Typography variant="h6" sx={{textAlign: 'center'}}>{subtitle}</Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  </>
+}
+
+
