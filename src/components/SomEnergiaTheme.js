d8cd17fb7d130fd3f738802588821554513425a9
David García Garzón <david.garcia@somenergia.coop>
2023-08-27 05:30:11 +0200

    ✨ styles from webforms-ui
    - Use a greeen that has more contrast with white letters

--- src/components/SomEnergiaTheme.js
+++ src/components/SomEnergiaTheme.js
@@ -7,11 +7,20 @@ export default function SomEnergiaTheme(isDarkMode) {
     },
     palette: {
       mode: (isDarkMode?'dark':'light'),
+      //contrastThreshold: 0.2, // From webforms-ui
+      contrastThreshold: 4.5, // Recommended by WCAG 2.1 Rule 1.4.3
+      tonalOffset: 0.5,
+      text: {
+        primary: (isDarkMode? '#bdbdbd':'#4d4d4d')
+      },
       primary: {
-        main: '#96D600', //'hsl(78, 100%, 42%)',
+        main: '#96b633', // from webforms-ui
+        //main: '#96D600', //'hsl(78, 100%, 42%)', // from style guide
+        contrastText: 'white',
       },
       secondary: {
-        main: '#E0E723', //'hsl(62, 80%, 52%)',
+        main: '#a1a1a1', // from webforms-ui
+        //main: '#E0E723', //'hsl(62, 80%, 52%)', // from style guide
       },
     },
   }))
