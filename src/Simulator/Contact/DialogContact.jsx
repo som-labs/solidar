fc199ff50c27f96299459a6314cbf9e2091eca77
joseluis-garcia <71826340+joseluis-garcia@users.noreply.github.com>
2023-12-14 08:34:42 +0100

    Contact form moved to Formix

--- src/Simulator/Contact/DialogContact.jsx
+++ src/Simulator/Contact/DialogContact.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 
 // MUI objects
 import {
@@ -10,7 +10,6 @@ import {
   FormControlLabel,
   Input,
   InputLabel,
-  Checkbox,
   FormLabel,
   RadioGroup,
   Radio,
@@ -20,166 +19,160 @@ import { DialogActions, DialogContent, DialogTitle } from '@mui/material'
 
 export default function DialogContact({ initialData, recoverFormData, onClose }) {
   const { t, i18n } = useTranslation()
-  const [formData, setFormData] = useState(initialData)
 
-  useEffect(() => {
-    console.log('INITIALDATA', initialData)
-    setFormData(initialData)
-  }, [initialData])
+  const validate = (values) => {
+    const errors = {}
+    console.log(values)
 
-  const handleInputChange = (e) => {
-    let { name, value } = e.target
-    if (name === 'mantenerContacto') value = e.target.checked
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-  }
+    if (values.nombre == '') {
+      errors.nombre = 'Requerido'
+    }
 
-  const handleEnd = (e) => {
-    e.preventDefault()
-    console.log('SENDINGDATABACK', e.target.id, formData)
-    recoverFormData(e.target.id, formData)
-    onClose(e.target.id)
+    if (!values.email) {
+      errors.email = 'Requerido'
+    } else {
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      if (!emailPattern.test(values.email)) errors.email = 'Invalid email address'
+    }
+
+    if (values.telefono) {
+      //Pattern for Spanish phone numbers (landline or mobile)
+      const patternSpain = /^(?:(?:\+|00)34)?[6-9]\d{8}$/
+      //Check if the phone number matches the pattern
+      if (!patternSpain.test(values.telefono))
+        errors.telefono = 'No es un número de teléfono correcto, verificalo por favor'
+    }
+
+    if (values.mensaje == '') {
+      errors.mensaje = 'Requerido'
+    }
+
+    console.log(errors)
+    return errors
   }
 
   return (
-    <>
-      <form id="save" onSubmit={handleEnd}>
-        <DialogTitle>{t('CONTACTO.DIALOG_TITLE')}</DialogTitle>{' '}
-        <DialogContent>
-          <Box
-            sx={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              flexDirection: 'column',
-              flex: 1,
-            }}
-          >
-            <Typography variant="body" align="center">
-              {t('CONTACTO.LABEL_descripcion')}
-            </Typography>
-
-            <FormControl variant="standard">
-              <InputLabel htmlFor="nombre">{t('CONTACTO.LABEL_nombre')}</InputLabel>
-              <Input
-                required
-                id="nombre"
-                name="nombre"
-                aria-describedby="tt_nombre"
-                value={formData.nombre}
-                onChange={handleInputChange}
-              />
-              {/* <FormHelperText id="tt_nombre">{t('contacto_LBL_nombre')}</FormHelperText> */}
-            </FormControl>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="email">{t('CONTACTO.LABEL_email')}</InputLabel>
-              <Input
-                required
-                id="email"
-                name="email"
-                type="email"
-                aria-describedby="tt_email"
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-              {/* <FormHelperText id="tt_email">{t('contacto_LBL_email')}</FormHelperText> */}
-            </FormControl>
-
-            <FormControl variant="standard">
-              <InputLabel htmlFor="telefono">{t('CONTACTO.LABEL_telefono')}</InputLabel>
-              <Input
-                id="telefono"
-                name="telefono"
-                type="phone"
-                aria-describedby="tt_telefono"
-                value={formData.telefono}
-                onChange={handleInputChange}
-              />
-              {/* <FormHelperText id="tt_email">{t('contacto_LBL_telefono')}</FormHelperText> */}
-            </FormControl>
-            <FormControl sx={{ mt: '1rem' }}>
-              <FormLabel id="demo-radio-buttons-group-label">
-                {t('CONTACTO.LABEL_tipoPropuesta')}
-              </FormLabel>
-              <RadioGroup
-                aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="error"
-                name="gridRadios"
-                onChange={handleInputChange}
-                value={formData.gridRadios}
+    <Formik
+      initialValues={initialData}
+      validate={validate}
+      onSubmit={(values, actions) => {
+        actions.setSubmitting(true)
+        alert(JSON.stringify(values, null, 2))
+        recoverFormData(values)
+        actions.setSubmitting(false)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <>
+          <Form>
+            <DialogTitle>{t('CONTACTO.DIALOG_TITLE')}</DialogTitle>{' '}
+            <DialogContent>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  flexDirection: 'column',
+                  flex: 1,
+                }}
               >
-                <FormControlLabel
-                  sx={{ ml: '3rem' }}
-                  value="error"
-                  control={<Radio />}
-                  label={t('CONTACTO.LABEL_error')}
+                <Typography variant="body" align="center">
+                  {t('CONTACTO.LABEL_descripcion')}
+                </Typography>
+
+                <InputLabel htmlFor="nombre">{t('CONTACTO.LABEL_nombre')}</InputLabel>
+                <Field name="nombre" as={Input}></Field>
+                <ErrorMessage name="nombre">{(msg) => <div>{msg}</div>}</ErrorMessage>
+
+                <InputLabel htmlFor="email">{t('CONTACTO.LABEL_email')}</InputLabel>
+                <Field name="email" as={Input}></Field>
+                <ErrorMessage name="email">{(msg) => <div>{msg}</div>}</ErrorMessage>
+
+                <InputLabel htmlFor="telefono">{t('CONTACTO.LABEL_telefono')}</InputLabel>
+                <Field name="telefono" as={Input}></Field>
+                <ErrorMessage name="telefono">{(msg) => <div>{msg}</div>}</ErrorMessage>
+
+                <FormControl></FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">
+                  {t('CONTACTO.LABEL_tipoPropuesta')}
+                </FormLabel>
+                <Field name="gridRadios">
+                  {({ field }) => (
+                    <RadioGroup
+                      {...field}
+                      name={field.name}
+                      value={field.value}
+                      onChange={field.onChange}
+                      defaultValue="error"
+                    >
+                      <FormControlLabel
+                        sx={{ ml: '3rem' }}
+                        control={<Radio />}
+                        value="error"
+                        label={t('CONTACTO.LABEL_error')}
+                      />
+                      <FormControlLabel
+                        sx={{ ml: '3rem', mt: '-0.5rem' }}
+                        control={<Radio />}
+                        value="mejora"
+                        label={t('CONTACTO.LABEL_mejora')}
+                      />
+                      <FormControlLabel
+                        sx={{ ml: '3rem', mt: '-0.5rem' }}
+                        control={<Radio />}
+                        value="comentario"
+                        label={t('CONTACTO.LABEL_comentario')}
+                      />
+                    </RadioGroup>
+                  )}
+                </Field>
+
+                <Typography
+                  variant="body"
+                  align="left"
+                  dangerouslySetInnerHTML={{
+                    __html: t('CONTACTO.LABEL_advertencia'),
+                  }}
                 />
-                <FormControlLabel
-                  sx={{ ml: '3rem', mt: '-0.5rem' }}
-                  value="mejora"
-                  control={<Radio />}
-                  label={t('CONTACTO.LABEL_mejora')}
-                />
-                <FormControlLabel
-                  sx={{ ml: '3rem', mt: '-0.5rem' }}
-                  value="comentario"
-                  control={<Radio />}
-                  label={t('CONTACTO.LABEL_comentario')}
-                />
-              </RadioGroup>
-            </FormControl>
-            <Typography
-              variant="body"
-              align="left"
-              dangerouslySetInnerHTML={{
-                __html: t('CONTACTO.LABEL_advertencia'),
-              }}
-            />
-            <FormControl>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="mantenerContacto"
-                    checked={formData.mantenerContacto}
-                    onChange={handleInputChange}
-                  />
-                }
-                label={t('CONTACTO.LABEL_respuesta')}
-              />
-            </FormControl>
-            <TextareaAutosize
-              required
-              value={formData.mensaje}
-              aria-label="minimum height"
-              minRows={3}
-              name="mensaje"
-              onChange={handleInputChange}
-              placeholder={t('CONTACTO.LABEL_mensaje')}
-              sx={{
-                width: '100%', // Adjust the width as needed
-                padding: '8px', // Adjust padding
-                fontSize: '16px', // Adjust font size
-                border: '1px solid', // Adjust border
-                borderRadius: '8px', // Adjust border radius
-                resize: 'vertical', // Allow vertical resizing only
-                '&:focus': {
-                  outline: 'none', // Remove outline on focus
-                  border: '1px solid blue', // Adjust border on focus
-                },
-              }}
-            />
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button id="cancel" onClick={handleEnd}>
-            {t('BASIC.LABEL_CANCEL')}
-          </Button>
-          <Button id="save" type="submit">
-            {t('BASIC.LABEL_SEND')}
-          </Button>
-        </DialogActions>
-      </form>
-    </>
+                <FormLabel>
+                  <Field type="checkbox" name="mantenerContacto" />
+                  {t('CONTACTO.LABEL_respuesta')}
+                </FormLabel>
+
+                {/* //REVISAR: como hacer para que respete el font */}
+                <Field name="mensaje">
+                  {({ field }) => (
+                    <TextareaAutosize
+                      {...field}
+                      aria-label="mensaje"
+                      placeholder={t('CONTACTO.LABEL_mensaje')}
+                      minRows={3}
+                      style={{
+                        width: '100%', // Adjust the width as needed
+                        padding: '8px', // Adjust padding
+                        // fontSize: '16px', // Adjust font size
+                        resize: 'vertical', // Allow vertical resizing only
+                        '&:focus': {
+                          outline: 'none', // Remove outline on focus
+                          border: '1px solid blue', // Adjust border on focus
+                        },
+                      }}
+                    />
+                  )}
+                </Field>
+                <ErrorMessage name="mensaje">{(msg) => <div>{msg}</div>}</ErrorMessage>
+              </Box>
+            </DialogContent>
+          </Form>
+          <DialogActions>
+            <Button id="cancel" onClick={onClose}>
+              {t('BASIC.LABEL_CANCEL')}
+            </Button>
+            <Button id="save" type="submit">
+              {t('BASIC.LABEL_SEND')}
+            </Button>
+          </DialogActions>
+        </>
+      )}
+    </Formik>
   )
 }
