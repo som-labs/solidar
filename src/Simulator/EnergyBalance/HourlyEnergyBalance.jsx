d84baa53049d025942c05867107cf4fb8276e23a
joseluis-garcia <71826340+joseluis-garcia@users.noreply.github.com>
2024-02-07 20:09:10 +0100

    First version of HourlyEnergyBalance for month
    ✨ HourlyEnergyBalance now allows mes as input argument

--- src/Simulator/EnergyBalance/HourlyEnergyBalance.jsx
+++ src/Simulator/EnergyBalance/HourlyEnergyBalance.jsx
@@ -12,25 +12,41 @@ import { Container } from '@mui/material'
 import * as UTIL from '../classes/Utiles'
 import TCB from '../classes/TCB'
 
-export default function HourlyEnergyBalance() {
+export default function HourlyEnergyBalance(props) {
   const { t } = useTranslation()
-
   const graphElement = useRef()
   const graphWidth = useRef()
   const maxHour = useRef()
   const minHour = useRef()
+  const { mes = '' } = props
+
+  let mProduccion
+  let mConsumo
 
   let hHora = []
   let hProduccion = []
   let hConsumo = []
+  let yProduccion = []
+  let yConsumo = []
+
   for (let hora = 0; hora < 24; hora++) {
     hHora.push(hora)
-    hProduccion.push(UTIL.suma(TCB.produccion.getHora(hora)) / 365)
-    hConsumo.push(UTIL.suma(TCB.consumo.getHora(hora)) / 365)
+    if (mes !== '') {
+      mProduccion = TCB.produccion
+        .getHora(hora)
+        .slice(UTIL.indiceDia[mes][1], UTIL.indiceDia[mes][2] + 1)
+      hProduccion.push(UTIL.promedio(mProduccion))
+      mConsumo = TCB.consumo
+        .getHora(hora)
+        .slice(UTIL.indiceDia[mes][1], UTIL.indiceDia[mes][2] + 1)
+      hConsumo.push(UTIL.promedio(mConsumo))
+    }
+    yProduccion.push(UTIL.promedio(TCB.produccion.getHora(hora)))
+    yConsumo.push(UTIL.promedio(TCB.consumo.getHora(hora)))
   }
 
-  maxHour.current = Math.max(Math.max(...hConsumo), Math.max(...hProduccion))
-  minHour.current = Math.min(Math.min(...hConsumo), Math.min(...hProduccion))
+  maxHour.current = Math.max(Math.max(...yConsumo), Math.max(...yProduccion))
+  minHour.current = Math.min(Math.min(...yConsumo), Math.min(...yProduccion))
   const delta = (maxHour.current - minHour.current) / 7
 
   useEffect(() => {
@@ -47,29 +63,74 @@ export default function HourlyEnergyBalance() {
     minHour.current = Math.min(Math.min(...hConsumo), Math.min(...hProduccion))
   }, [])
 
-  var trace1 = {
-    x: hHora,
-    y: hConsumo,
-    type: 'scatter',
-    line: { shape: 'spline', width: 3, color: '#4671ad' },
-    fill: 'tozeroy',
-    fillcolor: '#a1bee5',
-    name: t('Consumo.PROP.consumoHorario'),
-  }
+  let data
 
-  var trace2 = {
-    x: hHora,
-    y: hProduccion,
-    type: 'scatter',
-    line: { shape: 'spline', width: 3, color: '#ff9700' },
-    fill: 'tozeroy',
-    fillcolor: 'rgba(255,193,0,0.4)',
-    name: t('Produccion.PROP.produccionHoraria'),
+  if (mes === '') {
+    const consumoAnual = {
+      x: hHora,
+      y: yConsumo,
+      type: 'scatter',
+      line: { shape: 'spline', width: 3, color: '#4671ad' },
+      fill: 'tozeroy',
+      fillcolor: '#a1bee5',
+      name: t('ENERGY_BALANCE.LABEL_HOURLY_CONSUMPTION_YEAR'),
+    }
+
+    const produccionAnual = {
+      x: hHora,
+      y: yProduccion,
+      type: 'scatter',
+      line: { shape: 'spline', width: 3, color: '#ff9700' },
+      fill: 'tozeroy',
+      fillcolor: 'rgba(255,193,0,0.4)',
+      name: t('ENERGY_BALANCE.LABEL_HOURLY_PRODUCTION_YEAR'),
+    }
+    data = [consumoAnual, produccionAnual]
+  }
+  //Will compute average consumption and production per month
+  else {
+    const consumoAnual = {
+      x: hHora,
+      y: yConsumo,
+      type: 'scatter',
+      line: { shape: 'spline', width: 5, color: '#4671ad' },
+      name: t('ENERGY_BALANCE.LABEL_HOURLY_CONSUMPTION_YEAR'),
+    }
+
+    const produccionAnual = {
+      x: hHora,
+      y: yProduccion,
+      type: 'scatter',
+      line: { shape: 'spline', width: 5, color: '#ff9700' },
+      name: t('ENERGY_BALANCE.LABEL_HOURLY_PRODUCTION_YEAR'),
+    }
+
+    const consumoMes = {
+      x: hHora,
+      y: hConsumo,
+      type: 'scatter',
+      line: { shape: 'spline', width: 1, color: '#4671ad' },
+      fill: 'tozeroy',
+      fillcolor: 'rgba(161,120,0,0.4)', //'#a1bee5',
+      name: t('ENERGY_BALANCE.LABEL_HOURLY_CONSUMPTION_MONTH'),
+    }
+
+    const produccionMes = {
+      x: hHora,
+      y: hProduccion,
+      type: 'scatter',
+      line: { shape: 'spline', width: 1, color: '#ff9700' },
+      fill: 'tozeroy',
+      fillcolor: 'rgba(255,193,0,0.4)',
+      name: t('ENERGY_BALANCE.LABEL_HOURLY_PRODUCTION_MONTH'),
+    }
+
+    data = [consumoAnual, produccionAnual, consumoMes, produccionMes]
   }
 
   var layout = {
     legend: {
-      x: 0.3,
+      x: 0,
       xref: 'paper',
       y: -0.15,
       yref: 'paper',
@@ -99,7 +160,7 @@ export default function HourlyEnergyBalance() {
 
   return (
     <Container ref={graphElement}>
-      <Plot data={[trace1, trace2]} layout={layout} />
+      <Plot data={data} layout={layout} />
     </Container>
   )
 }
