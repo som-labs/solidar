3d1418e095dda992ac9cff6cc5711bf626165449
joseluis-garcia <71826340+joseluis-garcia@users.noreply.github.com>
2024-03-05 10:36:11 +0100

    Recovering images into PDF

--- src/Simulator/EnergyBalance/PieCharts.jsx
+++ src/Simulator/EnergyBalance/PieCharts.jsx
@@ -4,11 +4,12 @@ import { useEffect, useRef } from 'react'
 import Plot from 'react-plotly.js'
 
 // MUI objects
-import { Grid, Typography, Container, Box } from '@mui/material'
+import { Container } from '@mui/material'
 
 export default function PieCharts(props) {
   const graphElement = useRef()
   const graphWidth = useRef()
+  const { deficit, autoconsumo, excedente } = props.yearlyData
 
   useEffect(() => {
     // Function to get the width of the element
@@ -22,82 +23,57 @@ export default function PieCharts(props) {
     getWidth()
   }, [])
 
-  const isEmpty = Object.keys(props).length === 0
-  if (!isEmpty) {
-    const { consumo, produccion, deficit, autoconsumo, excedente } = props.yearlyData
-    // Sample data
-    const data1 = [
-      {
-        labels: ['Energia<br>de la red', 'Uso directo<br>de energía'],
-        values: [deficit, autoconsumo],
-        type: 'pie',
-        textinfo: 'percent+label',
-        insidetextorientation: 'horizontal',
-        textposition: 'outside',
-        hovertemplate: '<b>%{label}</b><br>%{percent}<br> %{value:.2f}kWh',
+  // Sample data
+  const data1 = [
+    {
+      labels: ['Energia<br>de la red', 'Uso directo<br>de energía'],
+      values: [deficit, autoconsumo],
+      marker: {
+        colors: ['#B6722F', '#FFFF66'],
       },
-    ]
+      type: 'pie',
+      textinfo: 'percent+label',
+      insidetextorientation: 'horizontal',
+      textposition: 'outside',
+      hovertemplate: '<b>%{label}</b><br>%{percent}<br> %{value:.2f}kWh',
+      domain: { row: 0, column: 0 },
+    },
+    //],
 
-    const data2 = [
-      {
-        labels: ['Vertido<br>a la red', 'Uso directo<br>de energía'],
-        values: [excedente, autoconsumo],
-        type: 'pie',
-        textinfo: 'percent+label',
-        insidetextorientation: 'horizontal',
-        textposition: 'outside',
-        hovertemplate: '<b>%{label}</b><br>%{percent}<br> %{value:.2f}kWh',
+    // const data2 = [
+    {
+      labels: ['Vertido<br>a la red', 'Uso directo<br>de energía'],
+      values: [excedente, autoconsumo],
+      marker: {
+        colors: ['#e15759', '#FFFF66'],
       },
-    ]
+      type: 'pie',
+      textinfo: 'percent+label',
+      insidetextorientation: 'horizontal',
+      textposition: 'outside',
+      hovertemplate: '<b>%{label}</b><br>%{percent}<br> %{value:.2f}kWh',
+      domain: { row: 0, column: 1 },
+    },
+  ]
 
-    const config = {
-      displayModeBar: false,
-    }
-
-    return (
-      <Container ref={graphElement}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            flexFlow: 'row',
-            flex: 2,
-            textAlign: 'center',
-          }}
-          justifyContent="center"
-        >
-          <Box sx={{ flex: 1 }}>
-            <Plot
-              data={data1}
-              layout={{
-                width: 0.45 * graphWidth.current,
-                height: 350,
-                title: 'Demanda Energética',
-                showlegend: false,
-                autosize: true,
-                paper_bgcolor: 'rgba(0,0,0,0)',
-                plot_bgcolor: 'rgba(0,0,0,0)',
-              }}
-              config={config}
-            />
-          </Box>
-          <Box sx={{ flex: 1 }}>
-            <Plot
-              data={data2}
-              layout={{
-                width: 0.45 * graphWidth.current,
-                height: 350,
-                title: 'Energía autoproducida',
-                showlegend: false,
-                autosize: true,
-                paper_bgcolor: 'rgba(0,0,0,0)',
-                plot_bgcolor: 'rgba(0,0,0,0)',
-              }}
-              config={config}
-            />
-          </Box>
-        </Box>
-      </Container>
-    )
+  const layout = {
+    width: graphWidth.current,
+    height: 350,
+    title: 'Demanda Energética',
+    showlegend: false,
+    autosize: true,
+    paper_bgcolor: 'rgba(0,0,0,0)',
+    plot_bgcolor: 'rgba(0,0,0,0)',
+    grid: { rows: 1, columns: 2 },
   }
+
+  const config = {
+    displayModeBar: false,
+  }
+
+  return (
+    <Container ref={graphElement}>
+      <Plot data={data1} layout={layout} config={config} />
+    </Container>
+  )
 }
